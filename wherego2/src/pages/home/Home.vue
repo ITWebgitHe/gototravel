5cc706095b0a4efaa78249a9002966a690bbbade
何绍府 <2734805265@163.com>
2019-08-12 11:04:25 +0800

    新建仓库的第一次提交项目

---
+++ wherego2/src/pages/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <HomeHeader/>
+		 <HomeSwiper/>
+		 
+  </div>
+</template>
+
+<script>
+ import HomeHeader from './components/Header'
+ import HomeSwiper from './components/swiper'
+  export default {
+    name:'Home',
+    components:{
+      HomeHeader,HomeSwiper
+    }
+   
+  }
+</script>
+
+<style >
+
+</style>
